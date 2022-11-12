0f5942770de763024648e5abb92df1f6dc5c0fe2
Serdar Mammadov <57108059+serdarm09@users.noreply.github.com>
2022-11-12 16:37:11 +0300

    odev2.1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace patikaodev2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int deger;
+            Console.Write("sayi giriniz:");
+            deger = Convert.ToInt32(Console.ReadLine());
+            string[] array = new string[deger];
+            for (int i = 0; i < deger; i++)
+            {
+                Console.Write("Kelimeyi giriniz:");
+                array[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Tersten yazdirilmis hali:");
+            for (int i = (deger - 1); i >= 0; i--)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
